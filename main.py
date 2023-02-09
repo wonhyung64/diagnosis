3f85cd932b90d4c49a63487216b725ece6871aa3
WonHyung Shin <wonhyung64@icloud.com>
2023-02-09 16:56:48 +0900

    feat - add finding fn function

--- main.py
+++ main.py
@@ -15,19 +15,73 @@ print(x)
 
 size = 3000
 x = random.normal(key, (size, size), dtype=jnp.float32)
-%timeit jnp.dot(x, x.T).block_until_ready()
+# %timeit jnp.dot(x, x.T).block_until_ready()
 
 x = np.random.normal(size=(size, size)).astype(np.float32)
-%timeit jnp.dot(x, x.T).block_until_ready()
+# %timeit jnp.dot(x, x.T).block_until_ready()
 
 
 x = np.random.normal(size=(size, size)).astype(np.float32)
 x = device_put(x)
-%timeit jnp.dot(x, x.T).block_until_ready()
+# %timeit jnp.dot(x, x.T).block_until_ready()
 
+
+def find_fn(gt_boxes, gt_labels, final_bboxes, final_labels, total_labels, iou_thresh=0.5):
+    fn_boxes = []
+    fn_labels = []
+    for c in range(total_labels):
+        # continue if no gt for c
+        if not tf.math.reduce_any(gt_labels == c):
+            continue
+
+        final_bbox = final_bboxes[final_labels == c]
+        gt_box = gt_boxes[gt_labels == c]
+        gt_label = gt_labels[gt_labels == c]
+        
+        # continue if no pred for c gt
+        if tf.shape(final_bbox)[0] == 0:
+            fn_boxes.append(gt_box)
+            fn_labels.append(gt_label)
+            continue
+
+        # if there are gt and final
+        iou = compute_iou(final_bbox, gt_box)
+        loc_match = iou >= iou_thresh
+        fn_bool = tf.logical_not(tf.reduce_any(loc_match, axis=0)).numpy()
+
+        fn_gt_box = gt_box[fn_bool]
+        fn_gt_label = gt_label[fn_bool]
+
+        fn_boxes.append(fn_gt_box)
+        fn_labels.append(fn_gt_label)
+
+    fn_boxes = tf.concat(fn_boxes, axis=0)
+    fn_labels = tf.concat(fn_labels, axis=0)
+
+    return fn_boxes, fn_labels
+
+
+def compute_iou(final_bbox, gt_box):
+    bbox_y1, bbox_x1, bbox_y2, bbox_x2 = tf.split(final_bbox, 4, axis=-1)
+    gt_y1, gt_x1, gt_y2, gt_x2 = tf.split(gt_box, 4, axis=-1)
+
+    bbox_area = (bbox_y2 - bbox_y1) * (bbox_x2 - bbox_x1)
+    gt_area = (gt_y2 - gt_y1) * (gt_x2 - gt_x1)
+
+    x_top = tf.maximum(bbox_x1, tf.transpose(gt_x1, [1, 0]))
+    y_top = tf.maximum(bbox_y1, tf.transpose(gt_y1, [1, 0]))
+    x_bottom = tf.minimum(bbox_x2, tf.transpose(gt_x2, [1, 0]))
+    y_bottom = tf.minimum(bbox_y2, tf.transpose(gt_y2, [1, 0]))
+
+    inter_area = tf.maximum(x_bottom - x_top, 0) * tf.maximum(y_bottom - y_top, 0)
+    union_area = (bbox_area + gt_area - inter_area)
+    iou = inter_area / union_area
+
+    return iou
 #%%
 # path argument
 path = "/Users/wonhyung64/data/diagnosis"
+total_labels = 21
 
 # def data loader
 samples = iter(os.listdir(path))
@@ -47,72 +101,11 @@ final_bboxes = json_load["final_bboxes"]
 final_labels = json_load["final_labels"]
 final_scores = json_load["final_scores"]
 
-def calculate_pr(final_bbox, gt_box, mAP_threshold):
-    bbox_num = tf.shape(final_bbox)[0]
-    gt_num = tf.shape(gt_box)[0]
 
-    true_pos = tf.Variable(tf.zeros(bbox_num))
-    for i in range(bbox_num):
-        bbox = tf.split(final_bbox, bbox_num, axis=1)[i]
-
-        iou = generate_iou(bbox, gt_box)
-
-        best_iou = tf.reduce_max(iou, axis=1)
-        pos_num = tf.cast(tf.greater(best_iou, mAP_threshold), dtype=tf.float32)
-        if tf.reduce_sum(pos_num) >= 1:
-            gt_box = gt_box * tf.expand_dims(
-                tf.cast(1 - pos_num, dtype=tf.float32), axis=-1
-            )
-            true_pos = tf.tensor_scatter_nd_update(true_pos, [[i]], [1])
-    false_pos = 1.0 - true_pos
-    true_pos = tf.math.cumsum(true_pos)
-    false_pos = tf.math.cumsum(false_pos)
-
-    recall = true_pos / gt_num
-    precision = tf.math.divide(true_pos, true_pos + false_pos)
-
-    return precision, recall
+fn_boxes, fn_labels = find_fn(gt_boxes, gt_labels, final_bboxes, final_labels, total_labels, iou_thresh=0.5)
+print(fn_labels)
+print(gt_labels)
+print(final_labels)
 
 
-def calculate_ap_per_class(recall, precision):
-    interp = tf.constant([i / 10 for i in range(0, 11)])
-    AP = tf.reduce_max(
-        [tf.where(interp <= recall[i], precision[i], 0.0) for i in range(len(recall))],
-        axis=0,
-    )
-    AP = tf.reduce_sum(AP) / 11
-    return AP
-
-IOU_THRESH = 0.5
-SCORE_THRESH = 0.3
-
-def calculate_ap_const(
-    final_bboxes, final_labels, gt_boxes, gt_labels, total_labels, mAP_threshold=0.5
-):
-    fn_boxes = []
-    fn_labels = []
-    for c in range(total_labels):
-        # continue if no gt for c
-        if not tf.math.reduce_any(gt_labels == c):
-            continue
-
-        final_bbox = final_bboxes[final_labels == c]
-        gt_box = gt_boxes[gt_labels == c]
-        gt_label = gt_labels[gt_labels == c]
-        
-
-        # continue if no pred for c gt
-        if tf.shape(final_bbox)[0] == 0:
-            fn_boxes.append(gt_box)
-            fn_labels.append(gt_label)
-            continue
-            
-        precision, recall = calculate_pr(final_bbox, gt_box, mAP_threshold)
-        ap = calculate_ap_per_class(recall, precision)
-        AP.append(ap)
-    if AP == []:
-        AP = 1.0
-    else:
-        AP = tf.reduce_mean(AP)
-    return AP
 
