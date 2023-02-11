5423bc8983a2dd45850dde24842626db3fa6b6e6
WonHyung Shin <wonhyung64@icloud.com>
2023-02-11 19:09:03 +0900

    feat - complete fn mechanism

--- main.py
+++ main.py
@@ -2,29 +2,30 @@
 import os
 import json
 import numpy as np
-import jax.numpy as jnp
+import pandas as pd
+# import jax.numpy as jnp
 import tensorflow as tf
-from jax import grad, jit, vmap
-from jax import random
-from jax import device_put
-
+# from jax import grad, jit, vmap
+# from jax import random
+# from jax import device_put
 # %%
-key = random.PRNGKey(0)
-x = random.normal(key, (10,))
-print(x)
+# key = random.PRNGKey(0)
+# x = random.normal(key, (10,))
+# print(x)
 
-size = 3000
-x = random.normal(key, (size, size), dtype=jnp.float32)
+# size = 3000
+# x = random.normal(key, (size, size), dtype=jnp.float32)
+# # %timeit jnp.dot(x, x.T).block_until_ready()
+
+# x = np.random.normal(size=(size, size)).astype(np.float32)
+# # %timeit jnp.dot(x, x.T).block_until_ready()
+
+
+# x = np.random.normal(size=(size, size)).astype(np.float32)
+# x = device_put(x)
 # %timeit jnp.dot(x, x.T).block_until_ready()
 
-x = np.random.normal(size=(size, size)).astype(np.float32)
-# %timeit jnp.dot(x, x.T).block_until_ready()
-
-
-x = np.random.normal(size=(size, size)).astype(np.float32)
-x = device_put(x)
-# %timeit jnp.dot(x, x.T).block_until_ready()
-
+#%%
 
 def find_fn(gt_boxes, gt_labels, final_bboxes, final_labels, total_labels, iou_thresh=0.5):
     fn_boxes = []
@@ -83,63 +84,86 @@ def compute_iou(final_bbox, gt_box):
 path = "/Users/wonhyung64/data/diagnosis"
 total_labels = 21
 
+#%%
 # def data loader
 samples = iter(os.listdir(path))
 
-sample = next(samples)
-with open(f"{path}/{sample}", encoding="UTF-8") as f:
-    json_load = json.load(f)
-json_load = {k: np.asarray(json_load[k]) for k in json_load.keys()}
+total_fn = []
 
-gt_boxes = json_load["gt_boxes"]
-gt_labels = json_load["gt_labels"]
-roi_bboxes = json_load["roi_bboxes"]
-roi_scores = json_load["roi_scores"]
-pred_bboxes = json_load["pred_bboxes"]
-pred_labels = json_load["pred_labels"]
-final_bboxes = json_load["final_bboxes"]
-final_labels = json_load["final_labels"]
-final_scores = json_load["final_scores"]
+while True:
+    sample = next(samples)
+    with open(f"{path}/{sample}", encoding="UTF-8") as f:
+        json_load = json.load(f)
+    json_load = {k: np.asarray(json_load[k]) for k in json_load.keys()}
 
+    gt_boxes = json_load["gt_boxes"]
+    gt_labels = json_load["gt_labels"]
+    roi_bboxes = json_load["roi_bboxes"]
+    roi_scores = json_load["roi_scores"]
+    pred_bboxes = json_load["pred_bboxes"]
+    pred_labels = json_load["pred_labels"]
+    final_bboxes = json_load["final_bboxes"]
+    final_labels = json_load["final_labels"]
+    final_scores = json_load["final_scores"]
 
-fn_boxes, fn_labels = find_fn(gt_boxes, gt_labels, final_bboxes, final_labels, total_labels, iou_thresh=0.5)
+    fn_boxes, fn_labels = find_fn(gt_boxes, gt_labels, final_bboxes, final_labels, total_labels, iou_thresh=0.5)
 
+    # fn mechanism
+    iou_thresh = 0.5
+    score_thresh = 0.3
 
-# fn mechanism
-iou_thresh = 0.5
-score_thresh = 0.3
+    iou_pred_fn = compute_iou(pred_bboxes, fn_boxes)
+    pred_iou_cond = iou_pred_fn >= iou_thresh
 
-iou_pred_fn = compute_iou(pred_bboxes, fn_boxes)
-pred_iou_cond = iou_pred_fn >= iou_thresh
+    # check cls 
+    fn_cls_problem = tf.reduce_any(pred_iou_cond, axis=[0, 1])
+    pred_cls_problem = tf.reduce_any(pred_iou_cond, axis=-1)
 
-# check cls 
-fn_cls_problem = tf.reduce_any(pred_iou_cond, axis=[0, 1])
-pred_cls_problem = tf.reduce_any(pred_iou_cond, axis=-1)
+    fn_cls_problem_boxes = fn_boxes[fn_cls_problem]
+    fn_cls_problem_labels = fn_labels[fn_cls_problem]
 
-fn_cls_problem_boxes = fn_boxes[fn_cls_problem]
-fn_cls_problem_labels = fn_labels[fn_cls_problem]
-
-fn_reg_problem_boxes = fn_boxes[tf.logical_not(fn_cls_problem)]
-fn_reg_problem_labels = fn_labels[tf.logical_not(fn_cls_problem)]
-
-
-# check cali, inter, bg
-s_loc = pred_labels[tf.logical_not(pred_cls_problem)] # logical_not 지워야 됨
-s_loc = tf.reshape(s_loc, [-1, total_labels])
-
-for c, b in zip(fn_cls_problem_labels, fn_cls_problem_boxes):  #fn_labels_1 으로 바꾸기
-    if tf.reduce_any(s_loc[..., c] >= score_thresh): print(1)
-    elif tf.reduce_any(s_loc[..., 0] >= score_thresh): print(3)
-    else: print(2)
+    fn_reg_problem_boxes = fn_boxes[tf.logical_not(fn_cls_problem)]
+    fn_reg_problem_labels = fn_labels[tf.logical_not(fn_cls_problem)]
 
 
-iou_roi_fn = compute_iou(roi_bboxes, fn_reg_problem_boxes)
-# check regressor, proposal
-roi_iou_cond = iou_roi_fn >= iou_thresh
-fn_reg_bool = tf.reduce_any(roi_iou_cond, axis=0)
+    # check cali, inter, bg
+    s_loc = pred_labels[tf.logical_not(tf.reduce_any(pred_cls_problem, axis=-1))] # logical_not 지워야 됨
 
-fn_reg_box = fn_reg_problem_boxes[fn_reg_bool]
-fn_reg_label = fn_reg_problem_labels[fn_reg_bool]
+    for c, b in zip(fn_cls_problem_labels, fn_cls_problem_boxes):  #fn_labels_1 으로 바꾸기
+        if tf.reduce_any(s_loc[..., c] >= score_thresh): 
+            fn_cali = [1.] + b.numpy().tolist() + [tf.cast(c, dtype=tf.float64).numpy()]
+            total_fn += [fn_cali]
+        elif tf.reduce_any(s_loc[..., 0] >= score_thresh): 
+            fn_bg = [3.] + b.numpy().tolist() + [tf.cast(c, dtype=tf.float64).numpy()]
+            total_fn += [fn_bg]
+        else: 
+            fn_inter = [2.] + b.numpy().tolist() + [tf.cast(c, dtype=tf.float64).numpy()]
+            total_fn += [fn_inter]
 
-fn_proposal_box = fn_reg_problem_boxes[tf.logical_not(fn_reg_bool)]
-fn_proposal_label = fn_reg_problem_labels[tf.logical_not(fn_reg_bool)]
+    iou_roi_fn = compute_iou(roi_bboxes, fn_reg_problem_boxes)
+    # check regressor, proposal
+    roi_iou_cond = iou_roi_fn >= iou_thresh
+    fn_reg_bool = tf.reduce_any(roi_iou_cond, axis=0)
+
+    fn_reg_box = fn_reg_problem_boxes[fn_reg_bool]
+    fn_reg_label = tf.expand_dims(fn_reg_problem_labels[fn_reg_bool], axis=-1)
+    if tf.shape(fn_reg_label)[0] != 0:
+        fn_reg = tf.concat([
+            tf.ones_like(fn_reg_label, dtype=tf.float64) * 4.,
+            fn_reg_box,
+            tf.cast(fn_reg_label, dtype=tf.float64)
+            ], axis=-1)
+        total_fn += fn_reg.numpy().tolist()
+
+
+    fn_proposal_box = fn_reg_problem_boxes[tf.logical_not(fn_reg_bool)]
+    fn_proposal_label = tf.expand_dims(fn_reg_problem_labels[tf.logical_not(fn_reg_bool)], axis=-1)
+    if tf.shape(fn_proposal_label)[0] != 0:
+        fn_proposal = tf.concat([
+            tf.ones_like(fn_proposal_label, dtype=tf.float64) * 5.,
+            fn_proposal_box,
+            tf.cast(fn_proposal_label, dtype=tf.float64)
+            ], axis=-1)
+        total_fn += fn_proposal.numpy().tolist()
+#%%
+fn_df = pd.DataFrame(total_fn, columns=["type", "box_y1", "box_x1", "box_y2", "box_x2", "label"])
