4c7a0e60213f8ac7766c3ce4746ec6d0cbe14452
wonhyung64 <wonhyung64@icloud.com>
2023-02-17 22:07:21 +0900

    feat - mmdetection attempting

--- detection.py
+++ detection.py
@@ -7,9 +7,20 @@ from module.mmdetection.mmdet.apis import init_detector, inference_detector
 mm_path = "./module/mmdetection"
 config_file = f"{mm_path}/yolov3_mobilenetv2_320_300e_coco.py"
 checkpoint_file = f"{mm_path}/yolov3_mobilenetv2_320_300e_coco_20210719_215349-d18dff72.pth"
-model = init_detector(config_file, checkpoint_file, device="cpu")
-model = init_detector(config_file, checkpoint_file, device="mps")
-inference_detector(model, f"{mm_path}/demo/demo.jpg")
 
+model = init_detector(config_file, checkpoint_file)
+model = init_detector(config_file, checkpoint_file, device="cpu")
+next(model.parameters())
+import time
+current = time.time()
+result = inference_detector(model, f"{mm_path}/demo/demo.jpg")
+result[2]
+len(result)
+result_img = model.show_result(f"{mm_path}/demo/demo.jpg", result)
+from torchvision import transforms
+transforms.ToPILImage()(result_img)
+print(time.time() - current)
+model.show_result()
 next(model.parameters()).device
 
+#%%
