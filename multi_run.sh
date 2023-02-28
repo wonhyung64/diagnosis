cfb66f342c807e1ed83a0be7813db62da8059321
Wonhyung Shin <wonhyung64@gmail.com>
2023-02-28 21:10:12 +0900

    feat - experimental settings

--- multi_run.sh
+++ multi_run.sh
@@ -14,10 +14,9 @@ DEFAULT_NAME=experiment
 
 # python argparse source for experiments
 experiments=(
-"--e=12 --b=33 --l=0.1"
-"--e=12 --b=33 --l=0.1"
-"--b=44 --l=0.2"
-"--e=14"
+"--config=retinanet_r18_fpn_1x_coco.py --checkpoint=retinanet_r18_fpn_1x_coco_20220407_171055-614fd399.pth"
+"--config=retinanet_r50_fpn_1x_coco.py --checkpoint=retinanet_r50_fpn_1x_coco_20200130-c2398f9e.pth"
+"--config=retinanet_x101_64x4d_fpn_1x_coco.py --checkpoint=retinanet_x101_64x4d_fpn_1x_coco_20200130-366f5af1.pth"
 )
 
 for index in ${!experiments[*]}; do
